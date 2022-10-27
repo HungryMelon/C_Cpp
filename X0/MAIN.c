5fc4441569fc8665973e0d3e5cbc0278bdef31b4
HungryMelon <91006918+HungryMelon@users.noreply.github.com>
2022-10-27 22:25:35 +0300

    Create MAIN.c

---
+++ X0/MAIN.c
@@ -0,0 +1,57 @@
+#include "stdio.h"
+#include "stdlib.h"
+#include "unistd.h"
+#include "wonCheck.c"
+// ═ ║ ╔ ╚ ╗ ╝ ╠ ╣ ╩ ╦ ╬
+
+void print_field(char field[3][3]){
+	printf("╔═══╦═══╦═══╗\n");
+	printf("║ %c ║ %c ║ %c ║\n", field[0][0], field[0][1], field[0][2]);
+	printf("╠═══╬═══╬═══╣\n");
+	printf("║ %c ║ %c ║ %c ║\n", field[1][0], field[1][1], field[1][2]);
+	printf("╠═══╬═══╬═══╣\n");
+	printf("║ %c ║ %c ║ %c ║\n", field[2][0], field[2][1], field[2][2]);
+	printf("╚═══╩═══╩═══╝\n");
+}
+
+int main(){
+	char score[] = {0, 0}; // 0 - X, 1 - 0
+	char field[3][3] = {
+		{'*', '*', '*'},
+		{'*', '*', '*'},
+		{'*', '*', '*'}
+	};
+	bool whoIswho = true; // X - true, 0 - false
+
+	while(true){ //global loop
+		char who = whoIswho == true ? 'X' : '0'; //who move now
+		int pos = 0;
+		//io:
+
+		printf("[X]: %d \t [0]: %d\n", score[0], score[1]);
+
+		print_field(field);
+
+		printf("Ход [%c]: ", who);
+
+		scanf("%d", &pos);
+		//
+		if ((pos % 10 > 0 || pos % 10 <= 3) && (pos / 10 > 0 || pos / 10 <= 3) && field[(pos / 10)-1][(pos % 10)-1] == '*'){
+			whoIswho = !whoIswho;
+			field[(pos / 10)-1][(pos % 10)-1] = who;
+		}
+		bool is_Won = isWon(field);
+
+		if (is_Won){
+			score[whoIswho]++;
+			for (int i = 0; i < 3; i++){
+				for (int j = 0; j < 3; j++){
+					field[i][j] = '*';
+				}
+			}
+		}
+		system("clear");
+		// printf("+----+\ndebug info \nprevious turn: \nis won: %d\nwho: %c\n+----+\n", is_Won, who); //debug info
+	}
+	return 0;
+}
