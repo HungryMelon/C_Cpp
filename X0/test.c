e27dcdae72b27ea0ca1085b5298227c90c283855
HungryMelon <91006918+HungryMelon@users.noreply.github.com>
2022-11-08 07:28:44 -0500

    test

--- X0/test.c
+++ X0/test.c
@@ -4,11 +4,20 @@
 #include "stdio.h"
 #include "bot.c"
 
+void print_field(char field[3][3]){
+	printf("╔═══╦═══╦═══╗\n");
+	printf("║ %c ║ %c ║ %c ║\n", field[0][0], field[0][1], field[0][2]);
+	printf("╠═══╬═══╬═══╣\n");
+	printf("║ %c ║ %c ║ %c ║\n", field[1][0], field[1][1], field[1][2]);
+	printf("╠═══╬═══╬═══╣\n");
+	printf("║ %c ║ %c ║ %c ║\n", field[2][0], field[2][1], field[2][2]);
+	printf("╚═══╩═══╩═══╝\n");
+}
 
 int main(){
 	char field[3][3] = {
-		{'X', '*', '*'},
-		{'*', '*', 'X'},
+		{'X', '*', 'X'},
+		{'*', '*', '*'},
 		{'*', '*', '*'}
 	};
 
@@ -18,4 +27,7 @@ int main(){
 	bool is_Won = isWon(field);
 	print_field(field);
 	printf("%d %d\n", robot, is_Won);
+
+	return 0;
 }
+
