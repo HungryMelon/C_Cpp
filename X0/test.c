3aa56949117d945ea4f0df0d17bd698caf4ee4b1
HungryMelon <91006918+HungryMelon@users.noreply.github.com>
2022-11-08 15:23:24 +0400

    test up

--- X0/test.c
+++ X0/test.c
@@ -1,32 +1,21 @@
 //test.c
 
 #include "wonCheck.c"
-#include "bot.c"
 #include "stdio.h"
+#include "bot.c"
 
-void print_field(char * field[3]){
-	printf("╔═══╦═══╦═══╗\n");
-	printf("║ %c ║ %c ║ %c ║\n", field[0][0], field[0][1], field[0][2]);
-	printf("╠═══╬═══╬═══╣\n");
-	printf("║ %c ║ %c ║ %c ║\n", field[1][0], field[1][1], field[1][2]);
-	printf("╠═══╬═══╬═══╣\n");
-	printf("║ %c ║ %c ║ %c ║\n", field[2][0], field[2][1], field[2][2]);
-	printf("╚═══╩═══╩═══╝\n");
-}
 
 int main(){
 	char field[3][3] = {
 		{'X', '*', '*'},
-		{'*', '*', '*'},
-		{'O', '*', 'O'}
+		{'*', '*', 'X'},
+		{'*', '*', '*'}
 	};
-	int pos = make_turn(field, 'X', 'O');
 
-	field[pos / 10][pos % 10] = 'X';
+	int robot = make_turn(field, 'X', 'O');
+	field[robot / 10][robot % 10] = 'X';
 
 	bool is_Won = isWon(field);
-
 	print_field(field);
-
-	printf("%d %d\n",pos, isWon(field));
+	printf("%d %d\n", robot, is_Won);
 }
