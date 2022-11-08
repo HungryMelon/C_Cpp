6856309382cc75d03619cfeb0c7856ffaadc0e41
HungryMelon <91006918+HungryMelon@users.noreply.github.com>
2022-11-08 19:39:56 +0000

    wonCheck up

--- X0/wonCheck.c
+++ X0/wonCheck.c
@@ -13,12 +13,12 @@ Win is posible if field look like:
 */
 int isWon(char field[3][3]){
 	for (int i = 0; i < 3; i++){ // 1
-		if ( (field[i][0] == field[i][1]) && (field[i][1] == field[i][2]) && (field[i][0] != '*')) return true;
+		if ((field[i][0] == field[i][1]) && (field[i][1] == field[i][2]) && (field[i][0] != '*' && field[i][0] != '-')) return true;
 	}
 	for (int i = 0; i < 3; i++){ // 2
-		if ( (field[0][i] == field[1][i]) && (field[1][i] == field[2][i]) && (field[0][i] != '*')) return true;
+		if ((field[0][i] == field[1][i]) && (field[1][i] == field[2][i]) && (field[0][i] != '*' && field[0][i] != '-')) return true;
 	}
-	if ((field[0][0] == field[1][1]) && (field[1][1] == field[2][2]) && (field[0][0] != '*')) return true; //3
-	if ((field[0][2] == field[1][1]) && (field[1][1] == field[2][0]) && (field[0][2] != '*')) return true; //4
+	if ((field[0][0] == field[1][1]) && (field[1][1] == field[2][2]) && ((field[0][0] != '*') && (field[0][0] != '-'))) return true; //3
+	if ((field[0][2] == field[1][1]) && (field[1][1] == field[2][0]) && ((field[0][2] != '*') && (field[0][2] != '-'))) return true; //4
 	return false;
 }
